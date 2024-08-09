ed3afe9ef6f6f09f1aeda3132ebbe173f136e5e9
Gabriel Gomes Attie <gabrielgattie@gmail.com>
2024-08-09 12:14:45 -0300

    aaaaa

---
+++ tests/test/conftest.py
@@ -0,0 +1,6 @@
+# tests/conftest.py
+import pytest
+
+@pytest.fixture
+def input_data():
+    return 5, 3
---
+++ tests/test/test_exampl.py
@@ -0,0 +1,9 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from app.exampl import add
+
+def test_add(input_data):
+    a, b = input_data
+    assert add(a, b) == 15
